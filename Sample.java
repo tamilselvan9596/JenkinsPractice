fdcb0c3f61d15d7e4d53c53e19c2dfa2819cedea
Tamil selvan <91212956+tamilselvan9596@users.noreply.github.com>
2024-07-23 01:26:42 +0530

    Create Sample.java

---
+++ Sample.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+      System.out.println("Hello, World!111");
+      System.out.println("Hello, World!222");
+      System.out.println("Hello, World!333");
+    }
+}
