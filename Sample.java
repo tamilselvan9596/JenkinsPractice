55f8a6c7207b81c2ed9d95cbb904351e34fb6368
Tamil selvan <91212956+tamilselvan9596@users.noreply.github.com>
2024-07-23 02:28:15 +0530

    Update Sample.java

--- Sample.java
+++ Sample.java
@@ -2,7 +2,7 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
       System.out.println("Hello, World!111");
-      System.out.println("Hello, World!222");
-      System.out.println("Hello, World!333");
+      System.out.println("Hello, World!222";
+     
     }
 }
